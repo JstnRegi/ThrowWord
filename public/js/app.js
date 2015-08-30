f8503fad9dced476fe46e4689827dc438ad47a8a
Justin Reyes <Justin@Justins-MacBook-Pro.local>
2015-08-30 12:36:24 -0700

    Basic specs for Catch Phrase project

--- public/js/app.js
+++ public/js/app.js
@@ -28,13 +28,30 @@ function getPhrases() {
     });
 }
 
+var words = [];
+var definitions = [];
 function renderPhrases(cps) {
+    //resets the arrays so they dont get overloaded every addition or removal of catchphrase
+    words = [];
+    definitions = [];
+
+    //makes the template out of our ul
     var template = _.template($('#cp-template').html());
     var cpList = cps.map(function(e) {
+        words.push(e.word);
         return template(e);
     });
+
+    //empties out the element so it isnt overloaded with catchphrases
     $('#catchphrases').empty();
+    //appends he cpList with map
     $('#catchphrases').append(cpList);
+
+    //gets a random number between 0 and the last of the index of the array
+    var randomNum = getRandomNum(0, (words.length - 1));
+
+    //appends a word into #word element
+    $('#word').append(words[randomNum]);
 }
 
 function addPhrase() {
@@ -64,4 +81,8 @@ function deletePhrase(context) {
 
 function toggleDefinition() {
     console.log('toggled')
+}
+
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
