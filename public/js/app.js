8bce53a00d60429846df5277d7bea60f105264bb
Justin Reyes <Justin@Justins-MacBook-Pro.local>
2015-08-31 23:46:48 -0700

    Added timers for both teams. Blinkers. Title. And adjusted some spacing

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,34 @@
+//how many seconds the clock has
+var seconds1 = 180;
+var seconds2 = 180;
+// for the sake of transition and to keep track when a second has passed to start the animation
+var tick = 0;
+
+//keypress values
+var yKey = 121;
+var nKey = 110;
+var enterKey = 13;
+
+//variables that keeps track who who's team it is
+var teamTurn = 1;
+var colorTicker = 1;
+
+//variables that keep track of score
+var team1ThumbsUp = 0;
+var team1ThumbsDown = 0;
+var team2ThumbsUp = 0;
+var team2ThumbsDown = 0;
+
+var countdownTimer1;
+var countdownTimer2;
+
+
 $(function() {
     pageLoad();
-    var seconds = 180;
-
-
-
 
 });
+
+
 
 function pageLoad() {
     //load phrases
@@ -31,19 +54,15 @@ function pageLoad() {
     toggleDefinitionsOff();
 
     $('#play-button').on('click', function() {
+        $('#score1').addClass('team-turn');
         console.log('click');
         $('#play-button').fadeOut('medium');
-            var countdownTimer = setInterval('secondPassed()', 1000);
-
-
+            countdownTimer1 = setInterval('secondPassed1()', 1000);
+            countdownTimer2 = setInterval('secondPassed2()', 1000);
     });
 
-
-
-
-
-
-
+    teamsTurn();
+    blinker();
 }
 
 function getPhrases() {
@@ -132,25 +151,76 @@ function getRandomNum(min, max) {
 }
 
 
-//how many seconds the clock has
-var seconds = 5;
-var tick = 0;
+function secondPassed1() {
 
-function secondPassed() {
-    var minutes = Math.round((seconds - 30)/60);
-    var remainingSeconds = seconds % 60;
+    var minutes = Math.round((seconds1 - 30)/60);
+    var remainingSeconds = seconds1 % 60;
     tick++;
 
     if (remainingSeconds < 10) {
         remainingSeconds = "0" + remainingSeconds;
     }
-    document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
-    if (seconds == 0) {
-        document.getElementById('countdown').innerHTML = "Buzz Buzz";
+    document.getElementById('countdown1').innerHTML = minutes + ":" + remainingSeconds;
+    if (seconds1 == 0) {
+        document.getElementById('countdown1').innerHTML = "Buzz Buzz";
     } else {
-        seconds--;
+        seconds1--;
     }
     if(tick === 1) {
-        $('#countdown').toggle('slow');
+        $('#countdown1').toggle('slow');
     }
+}
+
+function secondPassed2() {
+    console.log('running');
+    var minutes = Math.round((seconds2 - 30)/60);
+    var remainingSeconds = seconds2 % 60;
+
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+    }
+    document.getElementById('countdown2').innerHTML = minutes + ":" + remainingSeconds;
+    if (seconds2 == 0) {
+        document.getElementById('countdown2').innerHTML = "Buzz Buzz";
+    } else {
+        seconds2--;
+    }
+    if(tick === 1) {
+        $('#countdown2').toggle('slow', function(){
+            window.clearInterval(countdownTimer2);
+        });
+    }
+}
+
+function teamsTurn() {
+    $(window).on('keypress', function(e) {
+        if(e.which === enterKey) {
+            $('#score1').toggleClass('team-turn');
+            if(teamTurn % 2 !== 0) {
+                $('#score1').css('-webkit-text-stroke-width', '0px');
+                countdownTimer2 = setInterval('secondPassed2()', 1000);
+                window.clearInterval(countdownTimer1);
+            }
+            $('#right-side-title').toggleClass('team-turn');
+            if(teamTurn % 2 === 0) {
+                $('#right-side-title').css('-webkit-text-stroke-width', '0px');
+                countdownTimer1 = setInterval('secondPassed1()', 1000);
+                window.clearInterval(countdownTimer2);
+            }
+            teamTurn ++;
+        }
+    })
+}
+
+//Adds an effect around teams turn when it's their turn
+function blinker() {
+        setInterval(function() {
+            colorTicker += 1;
+            if (colorTicker % 2 === 0) {
+                $('.team-turn').css('-webkit-text-stroke-color', 'yellow').css('-webkit-text-stroke-width', '1px');
+            }
+            if (colorTicker % 2 !== 0) {
+                $('.team-turn').css('-webkit-text-stroke-color', '')
+            }
+        }, 400)
 }
