d7d4f2ddb996f8647812b817905f3b0c1e298056
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-10 18:55:42 -0700

    exported user model

--- models/index.js
+++ models/index.js
@@ -2,3 +2,4 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/catchphrase");
 
 module.exports.CatchPhrase = require("./phrases.js")
+module.exports.User = require("./users.js")
